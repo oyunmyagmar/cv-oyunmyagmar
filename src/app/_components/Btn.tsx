6810c69189b0905b8bb30d7550117f8922bbbe6d
oyunmyagmar <25LP4018@nest.edu.mn>
2026-02-15 02:18:16 +0900

    bio section added

--- src/app/_components/Btn.tsx
+++ src/app/_components/Btn.tsx
@@ -1,14 +1,33 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { IconType } from "react-icons";
 
-export const Btn = ({ text }: { text: string }) => {
+export const Btn = ({
+  text,
+  Icon,
+  size,
+  iconClassName = "",
+  href,
+}: {
+  text: string;
+  Icon: IconType;
+  size: number;
+  iconClassName?: string;
+  href: string;
+}) => {
   return (
-    <Button
-      size={"xs"}
-      variant={"ghost"}
-      className="cursor-pointer border-0  transition-all duration-500 ease-in-out"
-    >
-      {text}
-    </Button>
+    <div className="flex items-center justify-center gap-0.5">
+      <Button
+        asChild
+        size={"xs"}
+        variant={"ghost"}
+        className="cursor-pointer border-0 duration-500 ease-in-out text-[8px] h-fit px-0.5 py-0.5"
+      >
+        <a href={href}>{text}</a>
+      </Button>
+      <Icon
+        className={`text-[#0066C8] w-[14px] ${iconClassName}`}
+        size={size}
+      />
+    </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,53 @@
 import Link from "next/link";
 import { Btn } from "./_components";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { IoLogoGithub } from "react-icons/io5";
+import { HiMail } from "react-icons/hi";
 
 export default function HomePage() {
   return (
-    <div className="w-full h-screen flex justify-center py-8 px-11">
+    <div className="w-full h-screen flex justify-center py-8 px-11 flex-col gap-4 overflow-y-auto no-scrollbar">
       <div className="w-full flex justify-between">
         <div>
-          <div className="text-xl font-bold">Ганбаатар Оюунмягмар</div>
-          <div className="text-muted-foreground">Fullstack Developer</div>
+          <div className="text-xl font-semibold">Ганбаатар Оюунмягмар</div>
+          <div className="text-base text-muted-foreground">
+            Fullstack Developer
+          </div>
         </div>
-        <div className="flex flex-col text-xs items-end gap-0.5">
-          <a href="tel:+97699192615">
-            <Btn text="+976 99192615" />
-          </a>
-          <a href="mailto:oyunmyagmar.g@gmail.com">
-            <Btn text="oyunmyagmar.g@gmail.com" />
-          </a>
-          <Link href={"https://github.com/oyunmyagmar"}>
-            <Btn text="https://github.com/oyunmyagmar" />
-          </Link>
+        <div className="flex flex-col text-xs items-end">
+          <Btn
+            text="+976 99192615"
+            Icon={BsFillTelephoneFill}
+            size={11}
+            href="tel:+97699192615"
+          />
+          <Btn
+            text="oyunmyagmar.g@gmail.com"
+            Icon={HiMail}
+            size={13}
+            iconClassName="mt-[1.5px]"
+            href="mailto:oyunmyagmar.g@gmail.com"
+          />
+          <Btn
+            text="https://github.com/oyunmyagmar"
+            Icon={IoLogoGithub}
+            size={14}
+            iconClassName="mt-[0.5px]"
+            href="https://github.com/oyunmyagmar"
+          />
         </div>
       </div>
+      <div className="text-xs text-justify">
+        Системийн архитектур төлөвлөлтийг чухалчилж, хэрэглэгчийн үнэ цэнийг
+        эрхэмлэдэг Fullstack хөгжүүлэгч. 14+ төсөл (4 full-stack систем) дээр
+        ажилласан туршлагатай. Баг удирдах болон технологийн судалгаа хийх
+        (Real-time systems, Map GIS) чадвартай. Миний гол зорилго бол
+        технологийн оновчтой шийдлээр дамжуулан бизнесийн бодит асуудлыг
+        шийдвэрлэх бүтээгдэхүүн хөгжүүлэх юм.
+      </div>
+      <div className="text-[#0066C8] font-semibold">Ур чадвар</div>
+      <div className="text-[#0066C8] font-semibold">Туршлага</div>
+      <div className="text-[#0066C8] font-semibold">Боловсрол</div>
     </div>
   );
 }
